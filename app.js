d95cdc17e998b8a059c2306f4cc384966955ddaf
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-11-13 19:12:25 -0500

    Edits userController to allow userAuth for login and registration.

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 const app = express();
 const gamesController = require("./controllers/gamesController");
 const userController = require("./controllers/usersController")
+const favoritedGamesController = require("./controllers/favoritedGamesController")
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/games", gamesController)
 app.use("/users", userController);
+app.use("/favoritedGames", favoritedGamesController)
 
 app.get("/", (req, res) => {
   res.send("Welcome to our Game Store");
