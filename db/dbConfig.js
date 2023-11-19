a30b6b05e800ccfd9c8c2ff193a394942d6860e6
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-11-18 19:43:34 -0500

    test

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 
 
 const cn = {
-    host: process.env.PG_HOST, 
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
+    Hostname: process.env.PG_HOST, 
+    Port: process.env.PG_PORT,
+    Database: process.env.PG_DATABASE,
+    Username: process.env.PG_USER,
     PG_PASSWORD: process.env.PG_PASSWORD,
     DATABASE_URL: process.env.DATABASE_URL,
     PSQL_Command: process.env.PSQL_Command,
