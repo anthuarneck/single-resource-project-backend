2c9121236182fd66337224d09bd9ad8675e9b06a
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-11-18 22:48:55 -0500

    Edit dbConfig.

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,16 +1,18 @@
 const pgp = require("pg-promise")();
+require("dotenv").config();
 
-require('dotenv').config();
+const DATABASE_URL = process.env.DATABASE_URL 
 
-
-const cn = {
-    host: process.env.PG_HOST, 
+const cn = DATABASE_URL ? {
+    connectionString: DATABASE_URL,
+    max:30
+} : {
+    host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
     user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    databaseURL: process.env.DATABASE_URL
-};
+    password: process.env.PG_PASSWORD
+}
 
 const db = pgp(cn);
 
